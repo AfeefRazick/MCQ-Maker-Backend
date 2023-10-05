b567af656e3b0562a44659895a648725cf42c958
AfeefRazick <afeefrazickamir@gmail.com>
2023-10-05 15:57:53 +0530

    Serverless hosting using AWS Lambda

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const MCEModel = require("./models/mcqModel");
 const cors = require("cors");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
+const serverless = require("serverless-http");
 const { authenticate } = require("./middleware/auth");
 
-mongoose.connect(process.env.MONGODB_CONNECT_API_KEY_URL);
+let connection = null;
+
+const connectWithDB = async (req, res, next) => {
+  if (!connection) {
+    connection = await mongoose.connect(
+      process.env.MONGODB_CONNECT_API_KEY_URL
+    );
+    console.log("Connected to DB");
+  } else {
+    console.log(
+      "Connection already established, reusing the existing connection"
+    );
+  }
+  next();
+};
 
 app.use(express.json());
 app.use(cors());
+app.all("*", connectWithDB);
 app.use("/user/create", authenticate);
 
 const mceRoute = require("./routes/mce");
@@ -20,13 +35,20 @@ app.use("/mce", mceRoute);
 app.use("/user", userRoute);
 app.use("/mcqSubmission", mcqSubmissionRoute);
 
-app.listen(3002, () => {
-  console.log("poorgtrp");
-});
+const PORT = process?.env?.PORT || 3002;
+if (process?.env?.ENVIRONMENT === "production") {
+  module.exports.handler = serverless(app);
+  console.log("serverless");
+} else {
+  app.listen(PORT, () => {
+    console.log("development");
+    console.log(`Server is listening on port ${PORT}.`);
+  });
+}
 
-module.exports = {
-  express,
-  app,
-  dotenv,
-  mongoose,
-};
+// module.exports = {
+//   express,
+//   app,
+//   dotenv,
+//   mongoose,
+// };
